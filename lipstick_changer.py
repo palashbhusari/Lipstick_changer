616849a0c50d5f858acd2b410c7efffdf676060c
palashbhusari <palashshankarbhusari@gmail.com>
2019-11-29 19:19:16 +0530

    Final update- applying transperancy to the lips

--- lipstick_changer.py
+++ lipstick_changer.py
@@ -1,27 +1,32 @@
-
-###################### changing color using trackbars R G B
+#################### final upgrade- applying transperacy to the lips
 import cv2
 import numpy as np
 import dlib
 
-lip_color=(0,0,255)
+x=input("PRESS 'y' to start the lipstick app: ")
+print("****Starting Camera****")
+print("****Starting Application****")
+lip_color=(0,0,0)
+op=1
 ############################
 def nothing(x):
     pass
 # create trackbars for color change
 cv2.namedWindow('Frame')
-cv2.createTrackbar('R','Frame',0,255,nothing)
-cv2.createTrackbar('G','Frame',0,255,nothing)
-cv2.createTrackbar('B','Frame',0,255,nothing)
+cv2.createTrackbar('Red','Frame',0,255,nothing)
+cv2.createTrackbar('Green','Frame',0,255,nothing)
+cv2.createTrackbar('Blue','Frame',0,255,nothing)
+cv2.createTrackbar('Trans','Frame',25,50,nothing)
 
 
 def get_color():
     # get current positions of four trackbars
-    r = cv2.getTrackbarPos('R','Frame')
-    g = cv2.getTrackbarPos('G','Frame')
-    b = cv2.getTrackbarPos('B','Frame')
+    r = cv2.getTrackbarPos('Red','Frame')
+    g = cv2.getTrackbarPos('Green','Frame')
+    b = cv2.getTrackbarPos('Blue','Frame')
+    op = cv2.getTrackbarPos('Trans','Frame')
     #print((b,g,r))
-    return (b,g,r)
+    return (b,g,r),op/100
 #############################    
     
 cap = cv2.VideoCapture(0)
@@ -29,10 +34,13 @@ cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
+
 while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    orignal=frame.copy() # copy of the oringal image to over lay
+    
+     
     faces = detector(gray)
     for face in faces:
         x1 = face.left()
@@ -54,16 +62,24 @@ while True:
                  #(landmarks.part(60).x,landmarks.part(60).y),(0,0,255),1)# first points of lipsto last points of lips
         #cv2.line(frame,(landmarks.part(60).x,landmarks.part(60).y),
                  #(landmarks.part(67).x,landmarks.part(67).y),(0,0,255),1)     # draw line from first to last point
-    ###hull = cv2.convexHull(hull_points)
-        pts=np.asarray([hull_points])
-        cv2.fillPoly(frame, pts, lip_color)
-        
-    #print("hull = ",pts, type(pts))
 
-    lip_color=get_color()
-    #print(lip_color)
-    
-    cv2.imshow("Frame", frame)
+        pts=np.asarray([hull_points])
+        cv2.fillPoly(frame, pts, lip_color) # fill color in poly gon (lips)
+        
+        cv2.rectangle(frame, (0, 30), (60, 90),lip_color, -1) # create a rectable to display shade
+        
+        cv2.addWeighted(frame, op, orignal, 1-op,0, orignal) # transperancy
+        
+        
+        lip_color,op=get_color() # get color from track barr
+        
+        ###cv2.fillPoly(frame, shade, lip_color) # show shade of the color
+    #print(lip_color,'  ',op) # print color and opacity
+    #print("hull = ",pts, type(pts))
+        
+    cv2.putText(orignal,'SHADE',(0,25), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,0),2,cv2.LINE_AA)
+    cv2.imshow("Frame", orignal)
+    #cv2.imshow("rgb_img",rgb_img)
 
     key = cv2.waitKey(1)
     if key == 27:
